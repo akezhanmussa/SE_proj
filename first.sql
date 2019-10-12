6e8cd11af57db0a2b38ac64cc44431d884bdebb3
Daneker <daniker.ktl@gmail.com>
2019-10-12 20:16:58 +0600

    updated sql script

--- first.sql
+++ first.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2019-10-12 15:25
+-- Generated: 2019-10-12 20:15
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -9,23 +9,40 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE IF NOT EXISTS `mydb`.`Station` (
+CREATE SCHEMA IF NOT EXISTS `railway` DEFAULT CHARACTER SET gb18030 ;
+
+CREATE TABLE IF NOT EXISTS `railway`.`Train` (
+  `idTrain` INT(11) NOT NULL,
+  `status` TINYINT(4) NOT NULL,
+  `capacity` INT(11) NOT NULL,
+  `Station_idStation` INT(11) NOT NULL,
+  PRIMARY KEY (`idTrain`),
+  INDEX `fk_Train_Station1_idx` (`Station_idStation` ASC) VISIBLE,
+  CONSTRAINT `fk_Train_Station1`
+    FOREIGN KEY (`Station_idStation`)
+    REFERENCES `railway`.`Station` (`idStation`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = gb18030;
+
+CREATE TABLE IF NOT EXISTS `railway`.`Station` (
   `idStation` INT(11) NOT NULL,
   `station_name` VARCHAR(45) NOT NULL,
   `address` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idStation`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DEFAULT CHARACTER SET = gb18030;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`Route` (
+CREATE TABLE IF NOT EXISTS `railway`.`Route` (
   `idRoute` INT(11) NOT NULL,
   `start_time` DATETIME NOT NULL,
-  `end_time` DATETIME NOT NULL,
+  `send_time` DATETIME NOT NULL,
+  `passenger_number` INT(11) NOT NULL,
   `start_station_id` INT(11) NOT NULL,
   `end_station_id` INT(11) NOT NULL,
   `Schedule_idRoutes` INT(11) NOT NULL,
   `Train_idTrain` INT(11) NOT NULL,
-  `passenger_number` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idRoute`),
   INDEX `fk_Route_Station_idx` (`start_station_id` ASC) VISIBLE,
   INDEX `fk_Route_Station1_idx` (`end_station_id` ASC) VISIBLE,
@@ -33,40 +50,37 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Route` (
   INDEX `fk_Route_Train1_idx` (`Train_idTrain` ASC) VISIBLE,
   CONSTRAINT `fk_Route_Station`
     FOREIGN KEY (`start_station_id`)
-    REFERENCES `mydb`.`Station` (`idStation`)
+    REFERENCES `railway`.`Station` (`idStation`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Route_Station1`
     FOREIGN KEY (`end_station_id`)
-    REFERENCES `mydb`.`Station` (`idStation`)
+    REFERENCES `railway`.`Station` (`idStation`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Route_Schedule1`
     FOREIGN KEY (`Schedule_idRoutes`)
-    REFERENCES `mydb`.`Schedule` (`idRoutes`)
+    REFERENCES `railway`.`Schedule` (`idRoutes`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Route_Train1`
     FOREIGN KEY (`Train_idTrain`)
-    REFERENCES `mydb`.`Train` (`idTrain`)
+    REFERENCES `railway`.`Train` (`idTrain`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DEFAULT CHARACTER SET = gb18030;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`Schedule` (
+CREATE TABLE IF NOT EXISTS `railway`.`Schedule` (
   `idRoutes` INT(11) NOT NULL,
   `start_station_id` VARCHAR(45) NOT NULL,
   `end_station_id` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idRoutes`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-DROP TABLE IF EXISTS `mydb`.`TEAM` ;
-
-DROP TABLE IF EXISTS `mydb`.`PERSON` ;
+DEFAULT CHARACTER SET = gb18030;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
