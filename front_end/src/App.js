e246af70af2be5bd43c63842d58137394df84d5b
izatkhamiyev <izat0011@gmail.com>
2019-10-13 19:53:08 +0600

    redux configuration

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/ConfigureStore';
+
+const store = configureStore();
 
 function App() {
   return (
-    <Main/>
+      <Provider store={store}>
+        <Main/>
+      </Provider>
   );
 }
 
--- front_end/src/components/Main.js
+++ front_end/src/components/Main.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import Map from './Map';
+import DummySchedule from './DummySchedule'
+import {fetchSchedule} from '../redux/ScheduleActionCreators'
+
+
+const mapDispatchToProps = (dispatch) => ({
+    fetchSchedule: (path) => dispatch(fetchSchedule(path))
+});
+
+const mapStateToProps = (state) => ({
+    schedule: state.schedule
+});
 
 class Main extends Component{
     render() {
         return(
         <div className='container'>
             <div className='row justify-content-center'>
-                    <Map></Map>
+                <Map/>
             </div>
             <div className='row justify-content-center'>
-                <Search></Search>
+                <DummySchedule schedule={this.props.schedule} fetchSchedule={this.props.fetchSchedule}/>
             </div>
         </div>
         );
     }
 }
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
