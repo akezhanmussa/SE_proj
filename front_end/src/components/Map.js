05258a6efab79c9be169a4efa148538f9100889b
izatkhamiyev <izat0011@gmail.com>
2019-10-16 19:21:32 +0600

    trying to draw a map

--- front_end/src/components/Map.js
+++ front_end/src/components/Map.js
@@ -6,7 +6,12 @@ class Map extends Component {
     render() {
         return (
             <div>
-                There Should be a MAP
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Kazakhstan_provinces_and_province_capitals.svg/650px-Kazakhstan_provinces_and_province_capitals.svg.png" usemap="#image-map"/>
+
+                <map name="image-map">
+                    <area target="_blank" alt="" title="" href="" coords="395,103" shape="poly"/>
+                        <area target="" alt="" title="" href="" coords="" shape="0"/>
+                </map>
             </div>
         );
     }
