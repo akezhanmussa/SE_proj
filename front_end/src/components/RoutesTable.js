a4965a8ead50f4be8e93b62cfd356c2043ba0d33
izatkhamiyev <izat0011@gmail.com>
2019-10-19 18:01:05 +0600

    Train routes Modal

--- front_end/src/components/RoutesTable.js
+++ front_end/src/components/RoutesTable.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import Select from 'react-select';
 import Button from 'react-bootstrap/Button';
-import Form, {FormRow} from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form'
 import DatePicker from 'react-datepicker'
+import ScheduleTable from './ScheduleTable';
 import "react-datepicker/dist/react-datepicker.css";
 
 class RoutesTable extends Component{
@@ -30,12 +31,10 @@ class RoutesTable extends Component{
     }
 
     handleTimeRange = (timeRange) => {
-        console.log(timeRange)
         this.setState({timeRange})
     }
 
     handleSubmitResponse = (routes) => {
-        console.log(routes)
         this.setState({routes})
     }
 
@@ -45,80 +44,87 @@ class RoutesTable extends Component{
 
     render(){
         return(
-            <div className='search-box' style={{width: 500}}>
-                <Form>
-                    <Form.Row>
-                        <Form.Group className = "col-6">
-                            <Form.Label>Start Station</Form.Label>
-                            <Select
-                                name = "form-field-name"
-                                value = {this.state.startStation}
-                                onChange = {this.handleStartStChange}
-                                options = {[
-                                    {value: "3", label: "Astana"},
-                                    {value:"4", label: "Pavlodar"},
-                                    {value:"8", label: "Almaty"},
-                                    {value:"6", label: "Karaghandy"},
-                                    {value:"5", label: "Kokshetau"},
-                                    {value:"7", label: "Shymkent"}
-                                ]}
-                            />
-                        </Form.Group>
+            <div>
+                <div className='row justify-content-around'>
+                    <div className='search-box' style={{width: 500}}>
+                        <Form>
+                            <Form.Row>
+                                <Form.Group className = "col-6">
+                                    <Form.Label>Start Station</Form.Label>
+                                    <Select
+                                        name = "form-field-name"
+                                        value = {this.state.startStation}
+                                        onChange = {this.handleStartStChange}
+                                        options = {[
+                                            {value: "3", label: "Astana"},
+                                            {value:"4", label: "Pavlodar"},
+                                            {value:"8", label: "Almaty"},
+                                            {value:"6", label: "Karaghandy"},
+                                            {value:"5", label: "Kokshetau"},
+                                            {value:"7", label: "Shymkent"}
+                                        ]}
+                                    />
+                                </Form.Group>
 
-                        <Form.Group className = "col-6 ml-auto">
-                            <Form.Label>Destination Station</Form.Label>
-                            <Select
-                                name = "form-field-name"
-                                value = {this.state.destinationStation}
-                                onChange = {this.handleDestinationStChange}
-                                options = {[
-                                    {value:"3", label: "Astana"},
-                                    {value:"4", label: "Pavlodar"},
-                                    {value:"8", label: "Almaty"},
-                                    {value:"6", label: "Karaghandy"},
-                                    {value:"5", label: "Kokshetau"},
-                                    {value:"7", label: "Shymkent"}
-                                ]}
-                            />
-                        </Form.Group>
-                    </Form.Row>
+                                <Form.Group className = "col-6 ml-auto">
+                                    <Form.Label>Destination Station</Form.Label>
+                                    <Select
+                                        name = "form-field-name"
+                                        value = {this.state.destinationStation}
+                                        onChange = {this.handleDestinationStChange}
+                                        options = {[
+                                            {value:"3", label: "Astana"},
+                                            {value:"4", label: "Pavlodar"},
+                                            {value:"8", label: "Almaty"},
+                                            {value:"6", label: "Karaghandy"},
+                                            {value:"5", label: "Kokshetau"},
+                                            {value:"7", label: "Shymkent"}
+                                        ]}
+                                    />
+                                </Form.Group>
+                            </Form.Row>
 
-                    <Form.Row>
-                        <Form.Group className = "col-12">
-                            <p>Time range</p>
-                            <Select
-                                name = "form-field-name"
-                                isMulti
-                                className="basic-multi-select"
-                                value = {this.state.timeRange}
-                                onChange = {this.handleTimeRange}
-                                options = {[
-                                    {value: "afternoon", label: "Afternoon"},
-                                    {value: "night", label: "Night"},
-                                    {value: "morning", label: "Morning"},
-                                    {value: "evening", label: "Evening"}
-                                ]}
-                            />
-                        </Form.Group>
-                    </Form.Row>
+                            <Form.Row>
+                                <Form.Group className = "col-12">
+                                    <p>Time range</p>
+                                    <Select
+                                        name = "form-field-name"
+                                        isMulti
+                                        className="basic-multi-select"
+                                        value = {this.state.timeRange}
+                                        onChange = {this.handleTimeRange}
+                                        options = {[
+                                            {value: "afternoon", label: "Afternoon"},
+                                            {value: "night", label: "Night"},
+                                            {value: "morning", label: "Morning"},
+                                            {value: "evening", label: "Evening"}
+                                        ]}
+                                    />
+                                </Form.Group>
+                            </Form.Row>
 
-                    <Form.Row>
-                        <Form.Group className="col-8 d-flex">
-                            <p>Route Day</p>
-                            <DatePicker className='ml-2' selected = {this.state.timeRoute} onChange={this.handleTimeRoute} />
-                        </Form.Group>
-                        <Form.Group className='ml-auto mr-2'>
-                            <Button className='btn-secondary' onClick = {() => this.props.fetchSchedule({"origin": this.state.startStation.value,
-                                "destination": this.state.destinationStation.value, "date": this.state.timeRoute, "daytime": this.state.timeRange})}>
-                                Submit Route
-                            </Button>
-                        </Form.Group>
-                    </Form.Row>
+                            <Form.Row>
+                                <Form.Group className="col-8 d-flex">
+                                    <p>Route Day</p>
+                                    <DatePicker className='ml-2' selected = {this.state.timeRoute} onChange={this.handleTimeRoute} />
+                                </Form.Group>
+                                <Form.Group className='ml-auto mr-2'>
+                                    <Button className='btn-secondary' onClick = {() => this.props.fetchSchedule({"origin": this.state.startStation.value,
+                                        "destination": this.state.destinationStation.value, "date": this.state.timeRoute, "daytime": this.state.timeRange})}>
+                                        Submit Route
+                                    </Button>
+                                </Form.Group>
+                            </Form.Row>
 
-                    <Form.Row>
+                            <Form.Row>
 
-                    </Form.Row>
-                </Form>
+                            </Form.Row>
+                        </Form>
+                    </div>
+                </div>
+                <div className='row justify-content-around'>
+                    <ScheduleTable schedule={this.props.schedule}/>
+                </div>
             </div>
         )
     }
--- front_end/src/components/ScheduleTable.js
+++ front_end/src/components/ScheduleTable.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import {Loading} from './Loading';
+import {Modal, ModalBody, ModalHeader} from 'reactstrap';
+
+
 class ScheduleRow extends Component {
     render() {
         const id = this.props.id;
@@ -14,6 +17,13 @@ class ScheduleRow extends Component {
             <tr>
                 <td>
                     <div align='center'>
+                        <h5>{train}</h5>
+                        <button className='btn btn-secondary' onClick={() => this.callModal()}>Train route</button>
+                        <TrainRoute setClick={click => this.callModal = click} routes={this.props.routes}/>
+                    </div>
+                </td>
+                <td>
+                    <div align='center'>
                         <h5>{origin}</h5>
                         <h6>Departure Time: {startDate}</h6>
                     </div>
@@ -24,11 +34,7 @@ class ScheduleRow extends Component {
                         <h6>Arrival Time: {endDate}</h6>
                     </div>
                 </td>
-                <td>
-                    <div align='center'>
-                        <h5>{train}</h5>
-                    </div>
-                </td>
+
                 <td>
                     <div align='center'>
                         <h5 align='center'>{capacity}</h5>
@@ -70,12 +76,12 @@ class ScheduleTable extends Component{
                     startDate = {route.startTime}
                     endDate = {route.endTime}
                     handleBuy = {this.handleBuy}
+                    routes = {route.routes}
                 />
             );
         });
 
         if(!this.props.schedule.isLoading) {
-            console.log(this.props.schedule);
             if (this.props.schedule.schedule.length === 0 && this.props.schedule.req === 0)
                 return (
                     <div></div>
@@ -90,13 +96,13 @@ class ScheduleTable extends Component{
                         <thead>
                         <tr>
                             <th>
+                                <div align='center'>Train Number</div>
+                            </th>
+                            <th>
                                 <div align='center'>Origin</div>
                             </th>
                             <th>
                                 <div align='center'>Destination</div>
-                            </th>
-                            <th>
-                                <div align='center'>Train Number</div>
                             </th>
                             <th>
                                 <div align='center'>Free Spaces</div>
@@ -110,6 +116,92 @@ class ScheduleTable extends Component{
         else
             return <Loading/>
     }
+}
+
+class TrainRoute extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+
+    }
+    componentDidMount() {
+        this.props.setClick(this.toggleModal);
+    }
+
+    toggleModal() {
+        this.setState({isModalOpen: !this.state.isModalOpen});
+    }
+
+    render() {
+        return (
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal} >
+                    {this.props.routes[0].origin} -> {this.props.routes[this.props.routes.length - 1].destination}
+                </ModalHeader>
+                <ModalBody>
+                    <table className="table">
+                        <thead className="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Station</th>
+                            <th scope="col">Arrival Time</th>
+                            <th scope="col">Departure Time</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <RenderTrainRoutes routes={this.props.routes}/>
+                        </tbody>
+                    </table>
+                </ModalBody>
+            </Modal>
+        );
+    }
+
+}
+
+const RenderTrainRoutes = (props)=>{
+    const iter = []
+    var len = props.routes.length;
+    for(let i = 0; i < len; i++)
+    {
+        let route = props.routes[i];
+        let index = i;
+        let newRow =
+            <tr key={index}>
+                <td>
+                    {index + 1}
+                </td>
+                <td>
+                    {route.origin}
+                </td>
+                <td>
+                    {i == 0 ? "" : props.routes[i-1].endDate}
+                </td>
+                <td>
+                    {route.startDate}
+                </td>
+            </tr>
+        iter.push(newRow);
+    }
+    iter.push(
+        <tr key={len}>
+            <td>
+                {len + 1}
+            </td>
+            <td>
+                {props.routes[len-1].destination}
+            </td>
+            <td>
+                {props.routes[len-1].endDate}
+            </td>
+            <td>
+            </td>
+        </tr>);
+    return iter;
+
 }
 
 export default ScheduleTable;
