173231740790eb1928a170e1d98cc6eb19414949
izatkhamiyev <izat0011@gmail.com>
2019-10-30 21:23:07 +0600

    buy ticket page

--- front_end/src/components/Main.js
+++ front_end/src/components/Main.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import {fetchSchedule} from '../redux/ScheduleActionCreators'
 import Home from './Home';
-import {Loading} from './Loading'
+import BuyTicketForm from './BuyTicketForm';
+
 
 const mapDispatchToProps = (dispatch) => ({
     fetchSchedule: (path) => dispatch(fetchSchedule(path))
@@ -16,11 +17,28 @@ const mapStateToProps = (state) => ({
 
 class Main extends Component{
     render() {
+
+        const BuyTicket = ({match}) => {
+            let route = this.props.schedule.schedule.filter(route => route.id === parseInt(match.params.routeId, 10));
+            // if (route.length === 0)
+            //     return (
+            //         <div>
+            //             Select Ticket First
+            //         </div>
+            //     );
+            return (
+                <BuyTicketForm
+                    // route={route[0]}
+                />
+            );
+        }
+
+
         return (
             <div>
                 <Switch>
                     <Route path='/home' component={() => <Home fetchSchedule={this.props.fetchSchedule} schedule={this.props.schedule}/>}/>
-                    <Route path='/map' component={() => <Loading/>}/>
+                    <Route path='/buy_ticket/:routeId' component={BuyTicket}/>
                     <Redirect to='home'/>
                 </Switch>
 
