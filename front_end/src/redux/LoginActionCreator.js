a494618854b61c65d211048c5d8bc3014cedb329
DaDsoul <ake.mussa@gmail.com>
2019-11-01 04:46:07 +0600

    Added the akzhol jvt

--- front_end/src/redux/LoginActionCreator.js
+++ front_end/src/redux/LoginActionCreator.js
@@ -33,27 +33,25 @@ export function login(userData){
     console.log(userData)
     return dispatch => {
         dispatch(loginRequest(userData));
-        let token = "123";
-        localStorage.setItem('token', JSON.stringify(token));
-        localStorage.setItem('user', JSON.stringify(userData));
-        dispatch(loginApprove(token))
-        // fetch(loginUrl, {
-        //     method: 'POST',
-        //     headers: {'Content-Type':'application/json'},
-        //     body: JSON.stringify(userData)
-        // })
-        //     .then(res => res.json())
-        //     .then(res => {
-        //         if (res.error){
-        //             throw(res.error);
-        //         }
-        //         localStorage.setItem('token', JSON.stringify(res.token));
-        //         localStorage.setItem('user', JSON.stringify(userData));
-        //         dispatch(loginApprove(res.token))
-        //     })
-        //     .catch(err => {
-        //         dispatch(loginFailure(err.message));
-        //     })
+        fetch(loginUrl, {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(userData)
+        })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                if (res.token === ''){
+                    dispatch(loginFailure("Not such user exist"));
+                }else{
+                    localStorage.setItem('token', JSON.stringify(res.token));
+                    localStorage.setItem('user', JSON.stringify(userData));
+                    dispatch(loginApprove(res.token))
+                }
+            })
+            .catch(err => {
+                dispatch(loginFailure(err.message));
+            })
     }
 }
 
