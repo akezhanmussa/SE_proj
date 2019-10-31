28d8d870f429df1d17d8c28b428d86664848ac79
izatkhamiyev <izat0011@gmail.com>
2019-11-01 00:12:11 +0600

    merge with back

--- front_end/src/components/BuyTicketForm.js
+++ front_end/src/components/BuyTicketForm.js
@@ -91,7 +91,7 @@ class FillTicket extends Component{
             owner_document_id: this.docId.value,
             price: 0,
             start_date: this.props.route.startTime,
-            end_data: this.props.route.endTime,
+            end_date: this.props.route.endTime,
             owner_document_type: this.doctype.value,
             owner_firstname: this.firstame.value,
             owner_lastname: this.lastname.value
