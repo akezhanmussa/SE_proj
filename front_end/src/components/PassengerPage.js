076dedd86cf0a4eae14464f58a5a7ef26ae616fc
izatkhamiyev <izat0011@gmail.com>
2019-11-01 03:31:57 +0600

    remove navbar

--- front_end/src/components/PassengerPage.js
+++ front_end/src/components/PassengerPage.js
@@ -2,32 +2,32 @@ import React, {Component} from 'react';
 import {baseUrl} from "../shared/BaseUrl";
 import {Button} from "react-bootstrap";
 
-// const getPassengerData = (passenger) => {
-//     let body = {passengerId: passenger.token};
-//     return fetch(baseUrl + '/buyticket', {
-//         method: 'POST',
-//         headers: {'Content-Type':'application/json'},
-//         body: JSON.stringify(body)
-//     })
-//         .then(response => {
-//             console.log(response)
-//             if(response.ok){
-//                 return response.json();
-//             }
-//             else{
-//                 var error = new Error("Error " + response.status + ': ' + response.statusText);
-//                 error.response = response;
-//                 throw error;
-//             }
-//         })
-//         .catch (error => console.log("Error"));
-// };
+const getPassengerData = (passenger) => {
+    let body = {passengerId: passenger.token};
+    return fetch(baseUrl + '/buyticket', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(body)
+    })
+        .then(response => {
+            console.log(response)
+            if(response.ok){
+                return response.json();
+            }
+            else{
+                var error = new Error("Error " + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        })
+        .catch (error => console.log("Error"));
+};
 
 class PassengerPage extends Component{
     componentDidMount() {
-        // getPassengerData(this.props.passenger)
-        //     .then(response => this.passengerTickets = response)
-        //     .catch(err => console.log("err"));
+        getPassengerData(this.props.passenger)
+            .then(response => this.passengerTickets = response)
+            .catch(err => console.log("err"));
     }
 
     finishSession = () => {
