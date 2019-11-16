88670d0f9767633d81c3b1c8d9589fd3480108a4
DaDsoul <ake.mussa@gmail.com>
2019-11-16 18:02:38 +0600

    modified user form, get tickets is done by token checker

--- front_end/src/shared/BaseUrl.js
+++ front_end/src/shared/BaseUrl.js
@@ -2,3 +2,4 @@ export const baseUrl = "http://localhost:8080/railway_station_war/myrailway";
 export const registrationUrl = "http://localhost:8080/railway_station_war/myrailway/register";
 export const loginUrl = "http://localhost:8080/railway_station_war/myrailway/auth/login";
 export const getInfoUrl = "http://localhost:8080/railway_station_war/myrailway/mypage";
+export const getTiketsUrl = "http://localhost:8080/railway_station_war/myrailway/mypage/gettickets";
