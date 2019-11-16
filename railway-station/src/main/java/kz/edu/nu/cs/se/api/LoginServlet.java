424b2f9a8bf090c8824c6bee29e6e21a5dc62365
Daneker <daniker.ktl@gmail.com>
2019-11-16 17:12:21 +0600

    d

--- railway-station/src/main/java/kz/edu/nu/cs/se/api/LoginServlet.java
+++ railway-station/src/main/java/kz/edu/nu/cs/se/api/LoginServlet.java
@@ -20,8 +20,6 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-
-
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
