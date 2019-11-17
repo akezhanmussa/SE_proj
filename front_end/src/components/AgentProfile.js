525a3939d0c7b39a09a7872147920e7791dd146c
izatkhamiyev <izat0011@gmail.com>
2019-11-17 21:33:04 +0600

    agent pull tickets

--- front_end/src/components/AgentProfile.js
+++ front_end/src/components/AgentProfile.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import CreateTicketForAgent from './CreateTicketForAgent';
 import {Loading} from "./Loading";
 import {baseUrl, getInfoUrl} from "../shared/BaseUrl";
+import RoutesTable from "./RoutesTable";
+import {locations} from "../shared/Locations";
 class CreateTicket extends Component{
     constructor(props){
         super(props);
@@ -38,7 +40,10 @@ class Profile extends Component{
                     this.props.fetchProfile(res.body);
                     this.setState({isLoading: false});
                 })
-                .catch(error => this.setState({errMess: error}))
+                .catch(error => {
+                    this.setState({isLoading: false});
+                    this.setState({errMess: error.message});
+                })
         }
     }
 
@@ -46,7 +51,9 @@ class Profile extends Component{
         if(this.state.isLoading){
             return <Loading/>;
         }else if(this.state.errMess) {
-            return <div>{this.state.errMess}</div>;
+            return (
+                <div>{this.state.errMess}</div>
+            );
         }
         else{
             return (
@@ -59,20 +66,118 @@ class Profile extends Component{
 
 }
 
+class TicketPool extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoading: false,
+            errMess: null
+        }
+    }
+    componentDidMount() {
+        if(this.props.pullTickets.length === 0){
+            this.setState({isLoading: true});
+            fetch(baseUrl, {
+                method: 'POST',
+                headers: {'Content-Type':'application/json'},
+                body:JSON.stringify(this.props.admin.admin_token)
+            })
+                .then(res => res.json())
+                .then(res => {
+                    this.props.fetchPullTicket(res.body);
+                    this.setState({isLoading: false});
+                })
+                .catch(error => {
+                    this.setState({isLoading: false});
+                    this.setState({errMess: error.message});
+                })
+        }
+    }
+
+    render() {
+        if(this.state.isLoading){
+            return <Loading/>;
+        }else if(this.state.errMess) {
+            return (
+                <div>{this.state.errMess}</div>
+            );
+        }
+        else{
+            return (
+                <RenderTickets tickets={this.props.pullTickets} pull={true}/>
+            );
+        }
+    }
+}
+const RenderTickets = (props) => {
+    const intToStr = (id) => {
+        return locations.filter(loc => id === parseInt(loc.id))[0].name;
+    };
+    const tickets = props.tickets.map((ticket, idx) => {
+        return(
+            <div className='row mt-1' key={idx} style={{borderBottom: "1px solid #E7E7E7"}}>
+                <div className='col-3'>
+                    <p style={{textAlign:"center"}}><b>{intToStr(ticket.idOrigin)}</b></p>
+                    <p style={{textAlign:"center"}}>{ticket.startDate}</p>
+                </div>
+                <div className='col-3'>
+                    <p style={{textAlign:"center"}}><b>{intToStr(ticket.idDestination)}</b></p>
+                    <p style={{textAlign:"center"}}>{ticket.endDate}</p>
+                </div>
+                <div className='col-3'>
+                    <p style={{textAlign:"center"}}>{ticket.ownerFirstName + " " + ticket.ownerLastName}</p>
+                    <p style={{textAlign:"center"}}>{ticket.ownerDocumentId}</p>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        );
+    });
+    return(
+        <div className='mt-2'>
+            <div className='row mt-1' style={{borderBottom: "1px solid #E7E7E7"}}>
+                <div className='col-3'>
+                    <p style={{textAlign:"center"}}><b>Origin and start date</b></p>
+                </div>
+                <div className='col-3'>
+                    <p style={{textAlign:"center"}}><b>Destination and end date</b></p>
+                </div>
+                <div className='col-3'>
+                    <p style={{textAlign:"center"}}><b>Owner name and ID</b></p>
+                </div>
+                <div>
+                    <p style={{textAlign:"center"}}><b>You need to act</b></p>
+                </div>
+            </div>
+            {tickets}
+        </div>
+    );
+};
 
 class AgentProfile extends Component{
     constructor(props){
         super(props);
         this.state = {
-            profile: null
+            profile: null,
+            pullTickets: [{"idOrigin": 3,"idDestination": 7, "startDate":"2019","endDate":"2019", "ownerFirstName":"izat", "ownerLastName":"izat", "ownerDocumentId":12},
+                {"idOrigin": 3,"idDestination": 7, "startDate":"2019","endDate":"2019", "ownerFirstName":"izat", "ownerLastName":"izat", "ownerDocumentId":12}]
         };
         this.fetchProfile = this.fetchProfile.bind(this);
+        this.fetchPullTicket = this.fetchPullTicket.bind(this);
     }
 
     fetchProfile = (profile)=> {
         this.setState(prevState => ({
             ...prevState,
             profile: profile
+        }));
+    };
+
+    fetchPullTicket = (pull)=> {
+        this.setState(prevState => ({
+            ...prevState,
+            pullTickets: pull
         }));
     };
 
@@ -86,8 +191,10 @@ class AgentProfile extends Component{
                        role="tab" aria-controls="v-pills-profile" aria-selected="false">Update Profile</a>
                     <a className="nav-link" id="v-pills-create-ticket-tab" data-toggle="pill" href="#v-pills-create-ticket"
                        role="tab" aria-controls="v-pills-create-ticket" aria-selected="false">Create ticket</a>
-                    <a className="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings"
+                    <a className="nav-link" id="v-pills-my-tickets-tab" data-toggle="pill" href="#v-pills-my-tickets"
                        role="tab" aria-controls="v-pills-settings" aria-selected="false">My tickets</a>
+                    <a className="nav-link" id="v-pills-ticket-pull-tab" data-toggle="pill" href="#v-pills-ticket-pull"
+                       role="tab" aria-controls="v-pills-settings" aria-selected="false">Ticket Pool</a>
                 </div>
                 <div className="offset-1 tab-content col-8" id="v-pills-tabContent">
                     <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel"
@@ -101,11 +208,15 @@ class AgentProfile extends Component{
                          aria-labelledby="v-pills-messages-tab">
                         <CreateTicket/>
                     </div>
-                    <div className="tab-pane fade" id="v-pills-settings" role="tabpanel"
-                         aria-labelledby="v-pills-settings-tab">
+                    <div className="tab-pane fade" id="v-pills-my-tickets" role="tabpanel"
+                         aria-labelledby="v-pills-my-tickets-tab">
                         <button onClick={()=>console.log("fasd")}>
                             adsfsd
                         </button>
+                    </div>
+                    <div className="tab-pane fade" id="v-pills-ticket-pull" role="tabpanel"
+                         aria-labelledby="v-pills-ticket-pull-tab">
+                        <TicketPool pullTickets={this.state.pullTickets} fetchPullTicket={this.fetchPullTicket} admin={this.props.admin}/>
                     </div>
                 </div>
             </div>
